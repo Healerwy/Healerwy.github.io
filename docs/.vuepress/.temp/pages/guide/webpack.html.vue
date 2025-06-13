a549831a87bfee0de7508ffd2940f33f9134ff87
wangyue <2421447099@qq.com>
2025-06-13 11:27:52 +0800

    修改

--- docs/.vuepress/.temp/pages/guide/webpack.html.vue
+++ docs/.vuepress/.temp/pages/guide/webpack.html.vue
@@ -34,9 +34,37 @@
 </ul>
 </li>
 </ol>
-<h2 id="代码拆分-code-splitting" tabindex="-1"><a class="header-anchor" href="#代码拆分-code-splitting"><span>代码拆分（Code Splitting）</span></a></h2>
-<h2 id="资源模块-asset-modules" tabindex="-1"><a class="header-anchor" href="#资源模块-asset-modules"><span>资源模块（Asset Modules）</span></a></h2>
-<p>js可以引入css、图片等资源，webpack5之前需要使用file-loader、url-loader等loader，webpack5之后可以直接使用资源模块。</p>
+<h2 id="插件机制-plugins" tabindex="-1"><a class="header-anchor" href="#插件机制-plugins"><span>插件机制（Plugins）</span></a></h2>
+<p>解决资源加载以外其他自动化工作
+通过在生命周期的钩子中挂载函数实现拓展</p>
+<ol>
+<li>clean-webpack-plugin：清除打包前dist目录</li>
+<li>html-webpack-plugin：自动生成HTML文件，并引入打包后的资源</li>
+<li>SplitChunksPlugin：代码拆分，自动分割公共代码</li>
+</ol>
+<h2 id="dev-server" tabindex="-1"><a class="header-anchor" href="#dev-server"><span>Dev Server</span></a></h2>
+<p>提供用于开发的HTTP Server，集成自动编译和自动刷新浏览器等功能</p>
+<h2 id="source-map" tabindex="-1"><a class="header-anchor" href="#source-map"><span>Source Map</span></a></h2>
+<p>Source Map 是一种映射技术，可以将编译后的代码映射回源代码，方便调试。解决了源代码与运行代码不一致所产生的问题。</p>
+<ol>
+<li>cheap-mudule-eval-source-map：只包含行信息，不包含列信息，构建速度最快。</li>
+<li>nosources-source-map：不包含源代码，只包含行信息，可以保护源代码。</li>
+</ol>
+<h2 id="自动刷新" tabindex="-1"><a class="header-anchor" href="#自动刷新"><span>自动刷新</span></a></h2>
+<pre><code>1.模块热替换：HMR（Hot Module Replacement）在应用运行过程中替换、添加或删除模块，而无需重新加载整个页面。Webpack Dev Server 默认支持 HMR，只需在配置文件中启用即可。
+- 样式文件的热更新看起来开箱即用（style模块自行处理了）
+- js文件的热更新需要配置（注：通过脚手架创建的项目内部都继承了HMR方案）
+2.自动刷新：当文件发生变化时，自动刷新整个页面。Webpack Dev Server 默认支持自动刷新，只需在配置文件中设置 `open: true` 即可。
+</code></pre>
+<h2 id="生产环境优化" tabindex="-1"><a class="header-anchor" href="#生产环境优化"><span>生产环境优化</span></a></h2>
+<p>生产环境注重运行效率，开发环境注重开发效率。开发环境下配置的一些热部署、自动刷新等开发友好功能在生产环境下是不需要的，甚至是有害的。因此，我们需要在开发环境和生产环境之间进行配置分离。</p>
+<ol>
+<li>不同环境下的配置：</li>
+<li>不同环境的配置文件：</li>
+</ol>
+<h2 id="defineplugin" tabindex="-1"><a class="header-anchor" href="#defineplugin"><span>definePlugin</span></a></h2>
+<p>DefinePlugin 是 Webpack 提供的一个内置插件，用于在编译时创建全局常量。在代码中，可以通过 <code v-pre>process.env.NODE_ENV</code> 来访问这个常量。</p>
+<h2 id="tree-shaking" tabindex="-1"><a class="header-anchor" href="#tree-shaking"><span>Tree Shaking</span></a></h2>
 </div></template>
 
 
--- docs/guide/webpack.md
+++ docs/guide/webpack.md
@@ -22,8 +22,42 @@ loader是Webpack的核心特性,借助于Loader就可以加载任何类型的资
     - vue-loader:处理 Vue 单文件组件（.vue 文件）。
     - react-loader:处理 React 组件（.jsx、.tsx 文件）。
 
-## 代码拆分（Code Splitting）
+## 插件机制（Plugins）
 
-## 资源模块（Asset Modules）
+解决资源加载以外其他自动化工作
+通过在生命周期的钩子中挂载函数实现拓展
 
-js可以引入css、图片等资源，webpack5之前需要使用file-loader、url-loader等loader，webpack5之后可以直接使用资源模块。
+1. clean-webpack-plugin：清除打包前dist目录
+2. html-webpack-plugin：自动生成HTML文件，并引入打包后的资源
+3. SplitChunksPlugin：代码拆分，自动分割公共代码
+
+## Dev Server
+
+提供用于开发的HTTP Server，集成自动编译和自动刷新浏览器等功能
+
+## Source Map
+
+Source Map 是一种映射技术，可以将编译后的代码映射回源代码，方便调试。解决了源代码与运行代码不一致所产生的问题。
+
+1. cheap-mudule-eval-source-map：只包含行信息，不包含列信息，构建速度最快。
+2. nosources-source-map：不包含源代码，只包含行信息，可以保护源代码。
+
+## 自动刷新
+
+    1.模块热替换：HMR（Hot Module Replacement）在应用运行过程中替换、添加或删除模块，而无需重新加载整个页面。Webpack Dev Server 默认支持 HMR，只需在配置文件中启用即可。
+    - 样式文件的热更新看起来开箱即用（style模块自行处理了）
+    - js文件的热更新需要配置（注：通过脚手架创建的项目内部都继承了HMR方案）
+    2.自动刷新：当文件发生变化时，自动刷新整个页面。Webpack Dev Server 默认支持自动刷新，只需在配置文件中设置 `open: true` 即可。
+
+
+## 生产环境优化
+  生产环境注重运行效率，开发环境注重开发效率。开发环境下配置的一些热部署、自动刷新等开发友好功能在生产环境下是不需要的，甚至是有害的。因此，我们需要在开发环境和生产环境之间进行配置分离。
+
+  1. 不同环境下的配置：
+  2. 不同环境的配置文件：
+
+## definePlugin
+
+DefinePlugin 是 Webpack 提供的一个内置插件，用于在编译时创建全局常量。在代码中，可以通过 `process.env.NODE_ENV` 来访问这个常量。
+
+## Tree Shaking
