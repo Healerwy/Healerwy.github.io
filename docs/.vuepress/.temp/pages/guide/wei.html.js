05d1e2502af1b37e3a6b65deaefc30149798ad4a
wangyue <2421447099@qq.com>
2025-12-15 20:48:41 +0800

    xiugai

--- docs/.vuepress/.temp/pages/guide/wei.html.js
+++ docs/.vuepress/.temp/pages/guide/wei.html.js
@@ -1,5 +1,5 @@
 import comp from "D:/2025code/自主学习/学习文档网站/vuePress-demo/vuepress-document/Healerwy.github.io/docs/.vuepress/.temp/pages/guide/wei.html.vue"
-const data = JSON.parse("{\"path\":\"/guide/wei.html\",\"title\":\"微前端\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"git\":{\"updatedTime\":1764827897000,\"contributors\":[{\"name\":\"wangyue\",\"username\":\"\",\"email\":\"2421447099@qq.com\",\"commits\":1}],\"changelog\":[{\"hash\":\"cfe74c0cf7ae27fbf0cac410d855788c5f66b738\",\"time\":1764827897000,\"email\":\"2421447099@qq.com\",\"author\":\"wangyue\",\"message\":\"微前端\"}]},\"filePathRelative\":\"guide/wei.md\"}")
+const data = JSON.parse("{\"path\":\"/guide/wei.html\",\"title\":\"微前端\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"git\":{\"updatedTime\":1765782199000,\"contributors\":[{\"name\":\"wangyue\",\"username\":\"\",\"email\":\"2421447099@qq.com\",\"commits\":2}],\"changelog\":[{\"hash\":\"d35869ad4be3f1ce5f95b108fb410cee58b850cf\",\"time\":1765782199000,\"email\":\"2421447099@qq.com\",\"author\":\"wangyue\",\"message\":\"微前端\"},{\"hash\":\"cfe74c0cf7ae27fbf0cac410d855788c5f66b738\",\"time\":1764827897000,\"email\":\"2421447099@qq.com\",\"author\":\"wangyue\",\"message\":\"微前端\"}]},\"filePathRelative\":\"guide/wei.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
